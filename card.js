633386f83af8c72bd23b12dbcdd7e4b5de70122c
luismebratt <lmebratt@gmail.com>
2022-09-21 09:03:38 -0500

    ajuste color boton event click

--- card.js
+++ card.js
@@ -1,7 +1,7 @@
 let buy = document.getElementById("buy")
 
 function select(params) {
-    buy.style.background = "#112f58"
+    buy.style.background = "#086060"
 }
 
 buy.addEventListener("click", select)
